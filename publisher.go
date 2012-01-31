c2fa29b0d8178122c41d5e0367efafc586908167
Nick O'Neill <nick.oneill@gmail.com>
2012-01-31 10:08:11 -0800

    Added a large chunk of rebuild code, first basic checks for newer source files.

--- publisher.go
+++ publisher.go
@@ -9,9 +9,11 @@ import (
 	"strings"
 	// "path/filepath"
 	// "os"
+	"regexp"
 	"github.com/garyburd/go-oauth"
 	"github.com/nickoneill/go-dropbox"
-	// "launchpad.net/goyaml"
+	"launchpad.net/goyaml"
+	"github.com/hoisie/mustache.go"
 )
 
 const app_key = "ylg2zoaj78ol2dz"
@@ -20,11 +22,19 @@ const callback_url = "http://www.someurl.com/callback"
 
 var (
 	db = dropbox.NewClient(app_key, app_secret)
-	creds = new(oauth.Credentials)
+	lastbuild = time.Now()
+	// creds = new(oauth.Credentials)
 )
 
 type Chunk struct {
 	Command string
+}
+
+type Post struct {
+	Published bool
+	Title string
+	Date string
+	Content string
 }
 
 // main just loops and waits for jobs to return a command on a channel
@@ -52,30 +62,80 @@ func dropboxscrape(back chan *Chunk) {
 	for {
 		fmt.Printf("starting dropbox check\n")
 
-		back <- &Chunk{Command: "republish"}
+		source := db.GetFileMeta("source")
+		
+		needsrebuild := false
+		for _, textfile := range source.Contents {
+			// check each file for its modified date vs our last build date
+			changed, err := time.Parse(time.RFC1123Z, textfile.Modified)
+			if err != nil {
+				fmt.Printf("error parsing modified date %v",err)
+			}
+			
+			if lastbuild.Before(changed) {
+				needsrebuild = true
+			} else {
+				// nothing
+			}
+		}
+		
+		// TODO: only rebuild newer posts? here? in rebuild?
+		if needsrebuild {
+			back <- &Chunk{Command: "republish"}
+		}
 
-		fmt.Printf("sleeping dropbox check\n")
-		time.Sleep(10*time.Second)
+		time.Sleep(20*time.Second)
 	}
 }
 
 // basic rebuild command, builds site from dropbox files and deploys to configured location
 func rebuildSite() {
-	fmt.Printf("rebuilding %#v\n",db.Creds)
+	fmt.Printf("rebuilding\n")
+	
+	posttemplate, _ := db.GetFile("templates/post.mustache")
+	hometemplate, _ := db.GetFile("templates/home.mustache")
+	
+	posts := make([]Post,1)
 	
 	source := db.GetFileMeta("source")
 	for _, textfile := range source.Contents {
-		// fmt.Printf("text %v: %#v\n", i, text.Path)
-		if strings.Index(textfile.Path, " ") != -1 {
-			fmt.Printf("one of your files has a space in it! Fix it!\n")
-		} else {
-			text, err := db.GetFile(textfile.Path)
-			if err != nil {
-				fmt.Printf("error getting file text: %v",err)
+		// grab the contents of each source file
+		text, err := db.GetFile(textfile.Path)
+		if err != nil {
+			fmt.Printf("error getting file text: %v\n",err)
+		}
+
+		// parse yaml front matter, or don't act
+		if strings.HasPrefix(text, "---") {
+			p := Post{}
+			
+			parts := strings.SplitN(text, "---\n", 3)
+			
+			goyaml.Unmarshal([]byte(parts[1]), &p)
+			p.Content = parts[2]
+			// TODO: check for partial yaml and fill it
+			
+			// publish individual posts, ignore drafts
+			if p.Published {
+				posts = append(posts, p)
+				out := mustache.Render(posttemplate, map[string]string{"content": p.Content, "title": p.Title})
+				
+				pubpath := slugify(fmt.Sprintf("publish/%v.html",p.Title))
+				db.PutFile(pubpath, out)
+			} else {
+				fmt.Printf("\"%v\" is marked as draft, not publishing",p.Title)
 			}
-			fmt.Printf("got this file text: %v\n",text)
+		} else {
+			fmt.Printf("file with path \"%v\" is not a registered doc\n",textfile.Path)
+			// scrape should register new posts, not republish
 		}
 	}
+	
+	fmt.Printf("total posts: %v\n",len(posts))
+	out := mustache.Render(hometemplate, map[string]interface{}{"posts": posts})
+	
+	db.PutFile("publish/index.html",out)
+	fmt.Printf("Done publishing!\n")
 }
 
 func authDropbox() {
@@ -96,22 +156,20 @@ func authDropbox() {
 		err = save("config.json", newcreds.Token, newcreds.Secret)
 		db.Creds = newcreds
 	} else {
-		fmt.Printf("loaded creds: %v\n", savedcreds)
+		//fmt.Printf("loaded creds: %v\n", savedcreds)
 		db.Creds = savedcreds
-
-		// test for account info
-		// fmt.Printf("results: %v\n", drop.AccountInfo(creds))
-
-		// test for get file
-		// drop.GetFile(creds,"LLTP5.jpg")
-
-		// test for file meta
-		// newfilemeta := drop.GetFileMeta(creds, "folder")
-		// fmt.Printf("files: %#v\n", newfilemeta)
-		// for i, thing := range newfilemeta.Contents {
-		// 	fmt.Printf("file %v: %#v\n", i, thing)
-		// }
 	}
+}
+
+func slugify(orig string) string {
+	// removelist = [...]string{"a", "an", "as", "at", "before", "but", "by", "for","from","is", "in", "into", "like", "of", "off", "on", "onto","per","since", "than", "the", "this", "that", "to", "up", "via","with"}
+	
+	// for _, val := range removelist {
+	// 	remover = regexp.MustCompile("\b"+val+"\b")
+	// 	
+	// }
+	replaced := regexp.MustCompile("[\\s]").ReplaceAll([]byte(orig), []byte("_"))
+	return string(replaced)
 }
 
 func save(fileName string, accessToken string, accessSecret string) error {
