d0bedf5df3e03ea57cda64153eb0e721ab18201f
Nick O'Neill <nick.oneill@gmail.com>
2012-04-10 22:41:11 -0700

    Added demo pinboard feed url.

--- config.json.ex
+++ config.json.ex
@@ -1,6 +1,7 @@
 {
 	"DropboxKey":"PUTYERKEYHERE",
 	"DropboxSecret":"PUTYERSECRETHERE",
+	"PinboardFeedURL":"SET_A_PINBOARD_FEED_URL_TO_PULL_POSTS_FROM_PINBOARD",
 	"Rsync":{"Domain":"example.com","Username":"webuser","RemoteDir":"/var/www/example.com/public_html"},
 	"Publish":true}
 }
