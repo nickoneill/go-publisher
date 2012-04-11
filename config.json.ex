a636cdf11d733d7e3a4aefc72ae52d66955eabf3
Nick O'Neill <nick.oneill@gmail.com>
2012-04-10 22:34:13 -0700

    Updated config example

--- config.json.ex
+++ config.json.ex
@@ -1,4 +1,6 @@
 {
 	"DropboxKey":"PUTYERKEYHERE",
 	"DropboxSecret":"PUTYERSECRETHERE",
+	"Rsync":{"Domain":"example.com","Username":"webuser","RemoteDir":"/var/www/example.com/public_html"},
+	"Publish":true}
 }
